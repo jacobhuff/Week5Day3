af3e3223db96c68b796a12d09246edf979c4b01e
Jacob Huff <jacobhuff1313@gmail.com>
2020-10-29 09:44:56 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,80 @@
+/* Intermediate Algorithm Scripting: Sum All Odd Fibonacci Numbers */
+function sumFibs(num) {
+  var prevNumber = 0;
+  var currNumber = 1;
+  var result = 0;
+  while (currNumber <= num) {
+    if (currNumber % 2 !== 0) {
+      result += currNumber;
+    }
+
+    currNumber += prevNumber;
+    prevNumber = currNumber - prevNumber;
+  }
+
+  return result;
+}
+
+/* Intermediate Algorithm Scripting: Sum All Primes */
+function sumPrimes(num) {
+  var res = 0;
+
+  function getPrimes(max) {
+    var sieve = [];
+    var i;
+    var j;
+    var primes = [];
+    for (i = 2; i <= max; ++i) {
+      if (!sieve[i]) {
+        primes.push(i);
+        for (j = i << 1; j <= max; j += i) {
+          sieve[j] = true;
+        }
+      }
+    }
+
+    return primes;
+  }
+
+  var primes = getPrimes(num);
+  for (var p = 0; p < primes.length; p++) {
+    res += primes[p];
+  }
+
+  return res;
+}
+
+/* Intermediate Algorithm Scripting: Make a Person */
+var Person = function (firstAndLast) {
+  var fullName = firstAndLast;
+
+  this.getFirstName = function () {
+    return fullName.split(" ")[0];
+  };
+
+  this.getLastName = function () {
+    return fullName.split(" ")[1];
+  };
+
+  this.getFullName = function () {
+    return fullName;
+  };
+
+  this.setFirstName = function (name) {
+    fullName = name + " " + fullName.split(" ")[1];
+  };
+
+  this.setLastName = function (name) {
+    fullName = fullName.split(" ")[0] + " " + name;
+  };
+
+  this.setFullName = function (name) {
+    fullName = name;
+  };
+};
+
+/* TESTS */
+console.log(sumFibs(4));
+console.log(sumPrimes(10));
+var bob = new Person('Bob Ross');
+console.log(bob.getFullName());
